da43ec35b898b87a1c32a819b6ef3bba1e722291
mr_wind <mr_qhf@qq.com>
2016-07-27 09:47:43 +0800

    commit

--- app/src/main/java/com/carpediem/homer/demo/MainActivity.java
+++ app/src/main/java/com/carpediem/homer/demo/MainActivity.java
@@ -2,12 +2,43 @@ package com.carpediem.homer.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import com.carpediem.homer.funswitch.FunSwitch;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FunSwitch mFunSwitch;
+
+    private Button mSwitch;
+
+    private Switch mSwitch2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFunSwitch = (FunSwitch) findViewById(R.id.switchButton);
+        mFunSwitch.setOnSwitchChangeListener(new FunSwitch.OnSwitchChangedListener() {
+            @Override
+            public void onSwitchChange(FunSwitch funSwitch, boolean isChecked) {
+                Log.d("mr_wind", "isChecked: " + isChecked);
+            }
+        });
+        mSwitch = (Button) findViewById(R.id.switchbtn);
+        mSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mFunSwitch.isOpen()) {
+                    mFunSwitch.setChecked(false);
+                } else {
+                    mFunSwitch.setChecked(true);
+                }
+            }
+        });
     }
 }
--- funswitch/src/main/java/com/carpediem/homer/funswitch/FunSwitch.java
+++ funswitch/src/main/java/com/carpediem/homer/funswitch/FunSwitch.java
@@ -16,15 +16,17 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Interpolator;
+import android.widget.Checkable;
+import android.widget.CompoundButton;
 
 /**
  * Created by homer on 16-6-11.
  */
-public class FunSwitch extends View implements ValueAnimator.AnimatorUpdateListener,ValueAnimator.AnimatorListener{
+public class FunSwitch extends View implements ValueAnimator.AnimatorUpdateListener, ValueAnimator.AnimatorListener {
     private final static float DEFAULT_WIDTH_HEIGHT_PERCENT = 0.65f;
     private final static float FACE_ANIM_MAX_FRACTION = 1.4f;
     private final static float NORMAL_ANIM_MAX_FRACTION = 1.0f;
-    private float mWidthAndHeightPercent ;
+    private float mWidthAndHeightPercent;
     private float mWidth;
     private float mHeight;
     private float mPaddingLeft;
@@ -55,7 +57,9 @@ public class FunSwitch extends View implements ValueAnimator.AnimatorUpdateListe
     private boolean mIsDuringAnimation = false;
 
     private long mOnAnimationDuration = 850L;
-    private long mOffAnimationDuration = (long)(mOnAnimationDuration * NORMAL_ANIM_MAX_FRACTION / FACE_ANIM_MAX_FRACTION);
+    private long mOffAnimationDuration = (long) (mOnAnimationDuration * NORMAL_ANIM_MAX_FRACTION / FACE_ANIM_MAX_FRACTION);
+
+    private OnSwitchChangedListener mOnSwitchChangedListener;
 
     public FunSwitch(Context context) {
         super(context);
@@ -71,10 +75,11 @@ public class FunSwitch extends View implements ValueAnimator.AnimatorUpdateListe
         super(context, attrs, defStyleAttr);
         init(context);
     }
+
     private void init(Context context) {
         mWidthAndHeightPercent = DEFAULT_WIDTH_HEIGHT_PERCENT;
         mPaint = new Paint();
-        setState(false);
+        setChecked(false);
         // TODO: View ID 和 setSavedEnable都很重要的。
         setSaveEnabled(true);
     }
@@ -84,7 +89,7 @@ public class FunSwitch extends View implements ValueAnimator.AnimatorUpdateListe
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int width = MeasureSpec.getSize(widthMeasureSpec);
         int height = (int) (width * DEFAULT_WIDTH_HEIGHT_PERCENT);
-        setMeasuredDimension(width,height);
+        setMeasuredDimension(width, height);
     }
 
     @Override
@@ -95,29 +100,28 @@ public class FunSwitch extends View implements ValueAnimator.AnimatorUpdateListe
         mHeight = h;
         float top = 0;
         float left = 0;
-        float bottom = h*0.8f; //下边预留0.2空间来画阴影
+        float bottom = h * 0.8f; //下边预留0.2空间来画阴影
         float right = w;
 
-        RectF backgroundRecf = new RectF(left,top,bottom,bottom);
+        RectF backgroundRecf = new RectF(left, top, bottom, bottom);
         mBackgroundPath = new Path();
         //TODO:???????????
-        mBackgroundPath.arcTo(backgroundRecf,90,180);
-
+        mBackgroundPath.arcTo(backgroundRecf, 90, 180);
         backgroundRecf.left = right - bottom;
         backgroundRecf.right = right;
-        mBackgroundPath.arcTo(backgroundRecf,270,180);
+        mBackgroundPath.arcTo(backgroundRecf, 270, 180);
         mBackgroundPath.close();
 
         float radius = (bottom / 2) * 0.98f;
-        mCenterX =(top+bottom)/2;
-        mCenterY = (left+bottom)/2;
+        mCenterX = (top + bottom) / 2;
+        mCenterY = (left + bottom) / 2;
         mFaceRadius = radius;
         mTransitionLength = right - bottom;
 
-        RectF faceRecf = new RectF(mCenterX-mFaceRadius,mCenterY-mFaceRadius,mCenterX+mFaceRadius,mCenterY+mFaceRadius);
+        RectF faceRecf = new RectF(mCenterX - mFaceRadius, mCenterY - mFaceRadius, mCenterX + mFaceRadius, mCenterY + mFaceRadius);
         mFacePath = new Path();
-        mFacePath.arcTo(faceRecf,90,180);
-        mFacePath.arcTo(faceRecf,270,180);
+        mFacePath.arcTo(faceRecf, 90, 180);
+        mFacePath.arcTo(faceRecf, 270, 180);
 
     }
 
@@ -130,93 +134,98 @@ public class FunSwitch extends View implements ValueAnimator.AnimatorUpdateListe
 
     private void drawBackground(Canvas canvas) {
         mPaint.setColor(mCurrentColor);
+        mPaint.setAntiAlias(true);//背景绘制抗锯齿
         mPaint.setStyle(Paint.Style.FILL);
-        canvas.drawPath(mBackgroundPath,mPaint);
+        canvas.drawPath(mBackgroundPath, mPaint);
         mPaint.reset();
     }
+
     private void drawForeground(Canvas canvas) {
 
         //移动画布
         canvas.save();
-        canvas.translate(getForegroundTransitionValue(),0);
+        canvas.translate(getForegroundTransitionValue(), 0);
         drawFace(canvas, mAnimationFraction);
         canvas.restore();
     }
 
-    public void drawFace(Canvas canvas,float fraction) {
+    public void drawFace(Canvas canvas, float fraction) {
         mPaint.setAntiAlias(true);
         //面部背景
         mPaint.setColor(mFaceColor);
         mPaint.setStyle(Paint.Style.FILL);
-        canvas.drawPath(mFacePath,mPaint);
+        canvas.drawPath(mFacePath, mPaint);
 
-        translateAndClipFace(canvas,fraction);
-        drawEye(canvas,fraction);
-        drawMouth(canvas,fraction);
+        translateAndClipFace(canvas, fraction);
+        drawEye(canvas, fraction);
+        drawMouth(canvas, fraction);
     }
-    private void translateAndClipFace(Canvas canvas,float fraction) {
+
+    private void translateAndClipFace(Canvas canvas, float fraction) {
         //截掉超出face的部分。
         canvas.clipPath(mFacePath);
 
-        float faceTransition ;
+        float faceTransition;
         //TODO：合理的转动区间，眼睛出现和消失的时间比为1：1,所以当fraction=0.25时，应该只显示侧脸
-        if (fraction >=0.0f && fraction <0.5f) {
-            faceTransition = fraction * mFaceRadius *4;
-        } else if (fraction <=NORMAL_ANIM_MAX_FRACTION){
-            faceTransition = - (NORMAL_ANIM_MAX_FRACTION - fraction) * mFaceRadius * 4;
-        } else if (fraction <=(NORMAL_ANIM_MAX_FRACTION+FACE_ANIM_MAX_FRACTION)/2) {
-            faceTransition =  (fraction - NORMAL_ANIM_MAX_FRACTION) * mFaceRadius * 2;
+        if (fraction >= 0.0f && fraction < 0.5f) {
+            faceTransition = fraction * mFaceRadius * 4;
+        } else if (fraction <= NORMAL_ANIM_MAX_FRACTION) {
+            faceTransition = -(NORMAL_ANIM_MAX_FRACTION - fraction) * mFaceRadius * 4;
+        } else if (fraction <= (NORMAL_ANIM_MAX_FRACTION + FACE_ANIM_MAX_FRACTION) / 2) {
+            faceTransition = (fraction - NORMAL_ANIM_MAX_FRACTION) * mFaceRadius * 2;
         } else {
             faceTransition = (FACE_ANIM_MAX_FRACTION - fraction) * mFaceRadius * 2;
         }
 
-        canvas.translate(faceTransition,0);
+        canvas.translate(faceTransition, 0);
     }
 
-    private void drawEye(Canvas canvas,float fraction) {
+    private void drawEye(Canvas canvas, float fraction) {
 
         float scale;
         float startValue = 1.2f;
-        float middleValue = (startValue + FACE_ANIM_MAX_FRACTION) /2; //1.3
+        float middleValue = (startValue + FACE_ANIM_MAX_FRACTION) / 2; //1.3
         if (fraction >= startValue && fraction <= middleValue) {
             scale = (middleValue - fraction) * 10; //0.4f是最小缩放比
         } else if (fraction > middleValue && fraction <= FACE_ANIM_MAX_FRACTION) {
-            scale = (fraction - middleValue) * 10 ;
+            scale = (fraction - middleValue) * 10;
         } else {
             scale = 1.0f;
         }
 
         // 双眼
-        Log.e("SACLE","scale is "+scale);
-        float eyeRectWidth = mFaceRadius * 0.25f ;
+        Log.e("SACLE", "scale is " + scale);
+        float eyeRectWidth = mFaceRadius * 0.25f;
         float eyeRectHeight = mFaceRadius * 0.35f;
         float eyeXOffSet = mFaceRadius * 0.14f;
         float eyeYOffSet = mFaceRadius * 0.12f;
-        float leftEyeCenterX = mCenterX - eyeXOffSet - eyeRectWidth/2;
-        float leftEyeCenterY = mCenterY - eyeYOffSet - eyeRectHeight/2;
-        float rightEyeCenterX = mCenterX + eyeXOffSet + eyeRectWidth/2;
+        float leftEyeCenterX = mCenterX - eyeXOffSet - eyeRectWidth / 2;
+        float leftEyeCenterY = mCenterY - eyeYOffSet - eyeRectHeight / 2;
+        float rightEyeCenterX = mCenterX + eyeXOffSet + eyeRectWidth / 2;
 
         eyeRectHeight *= scale; //眨眼缩放
-        float eyeLeft = leftEyeCenterX - eyeRectWidth/2 ;
-        float eyeTop = leftEyeCenterY - eyeRectHeight/2;
-        float eyeRight = leftEyeCenterX + eyeRectWidth/2;
-        float eyeBottom = leftEyeCenterY + eyeRectHeight/2;
+        float eyeLeft = leftEyeCenterX - eyeRectWidth / 2;
+        float eyeTop = leftEyeCenterY - eyeRectHeight / 2;
+        float eyeRight = leftEyeCenterX + eyeRectWidth / 2;
+        float eyeBottom = leftEyeCenterY + eyeRectHeight / 2;
 
-        RectF leftEye = new RectF(eyeLeft,eyeTop,eyeRight,eyeBottom);
+        RectF leftEye = new RectF(eyeLeft, eyeTop, eyeRight, eyeBottom);
 
-        eyeLeft = rightEyeCenterX - eyeRectWidth/2;
-        eyeRight = rightEyeCenterX + eyeRectWidth/2;
+        eyeLeft = rightEyeCenterX - eyeRectWidth / 2;
+        eyeRight = rightEyeCenterX + eyeRectWidth / 2;
 
-        RectF rightEye = new RectF(eyeLeft,eyeTop,eyeRight,eyeBottom);
+        RectF rightEye = new RectF(eyeLeft, eyeTop, eyeRight, eyeBottom);
 
         mPaint.setColor(mCurrentColor);
         mPaint.setStyle(Paint.Style.FILL);
         //眨眼动画
 
-        canvas.drawOval(leftEye,mPaint);
-        canvas.drawOval(rightEye,mPaint);
+        canvas.drawOval(leftEye, mPaint);
+        canvas.drawOval(rightEye, mPaint);
+
     }
-    private void drawMouth(Canvas canvas,float fraction) {
+
+    private void drawMouth(Canvas canvas, float fraction) {
         //TODO:使用贝塞尔曲线来画嘴
         float eyeRectWidth = mFaceRadius * 0.2f;
         float eyeXOffSet = mFaceRadius * 0.14f;
@@ -227,16 +236,16 @@ public class FunSwitch extends View implements ValueAnimator.AnimatorUpdateListe
         float mouthTop = mCenterY + eyeYOffSet; // mCenterY是face的原点
 
         //嘴巴
-        if (fraction <=0.75) { //
+        if (fraction <= 0.75) { //
             canvas.drawRect(mouthLeft, mouthTop, mouthLeft + mouthWidth, mouthTop + mouthHeight, mPaint);
         } else {
             Path path = new Path();
-            path.moveTo(mouthLeft,mouthTop);
-            float controlX = mouthLeft + mouthWidth/2;
+            path.moveTo(mouthLeft, mouthTop);
+            float controlX = mouthLeft + mouthWidth / 2;
             float controlY = mouthTop + mouthHeight + mouthHeight * 15 * (fraction - 0.75f);
-            path.quadTo(controlX,controlY,mouthLeft+mouthWidth,mouthTop);
+            path.quadTo(controlX, controlY, mouthLeft + mouthWidth, mouthTop);
             path.close();
-            canvas.drawPath(path,mPaint);
+            canvas.drawPath(path, mPaint);
         }
     }
 
@@ -258,9 +267,10 @@ public class FunSwitch extends View implements ValueAnimator.AnimatorUpdateListe
         }
         return result;
     }
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        Log.e("TEST","onTouchEvent");
+        Log.e("TEST", "onTouchEvent");
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 return true;
@@ -277,10 +287,18 @@ public class FunSwitch extends View implements ValueAnimator.AnimatorUpdateListe
                     startOpenAnimation();
                     mIsOpen = true;
                 }
+                if (mOnSwitchChangedListener != null) {
+                    mOnSwitchChangedListener.onSwitchChange(this, mIsOpen);
+                }
                 return true;
         }
         return super.onTouchEvent(event);
     }
+
+    public boolean isOpen() {
+        return mIsOpen;
+    }
+
     private void startOpenAnimation() {
         mValueAnimator = ValueAnimator.ofFloat(0.0f, FACE_ANIM_MAX_FRACTION);
         mValueAnimator.setDuration(mOnAnimationDuration);
@@ -291,8 +309,9 @@ public class FunSwitch extends View implements ValueAnimator.AnimatorUpdateListe
         startColorAnimation();
 
     }
+
     private void startCloseAnimation() {
-        mValueAnimator = ValueAnimator.ofFloat(NORMAL_ANIM_MAX_FRACTION,0);
+        mValueAnimator = ValueAnimator.ofFloat(NORMAL_ANIM_MAX_FRACTION, 0);
         mValueAnimator.setDuration(mOffAnimationDuration);
         mValueAnimator.addUpdateListener(this);
         mValueAnimator.addListener(this);
@@ -300,10 +319,11 @@ public class FunSwitch extends View implements ValueAnimator.AnimatorUpdateListe
         mValueAnimator.start();
         startColorAnimation();
     }
+
     private void startColorAnimation() {
-        int colorFrom = mIsOpen?mOnBackgroundColor:mOffBackgroundColor; //mIsOpen为true则表示要启动关闭的动画
-        int colorTo = mIsOpen? mOffBackgroundColor:mOnBackgroundColor;
-        long duration = mIsOpen? mOffAnimationDuration:mOnAnimationDuration;
+        int colorFrom = mIsOpen ? mOnBackgroundColor : mOffBackgroundColor; //mIsOpen为true则表示要启动关闭的动画
+        int colorTo = mIsOpen ? mOffBackgroundColor : mOnBackgroundColor;
+        long duration = mIsOpen ? mOffAnimationDuration : mOnAnimationDuration;
         ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
         colorAnimation.setInterpolator(mInterpolator);
         colorAnimation.setDuration(duration); // milliseconds
@@ -311,17 +331,18 @@ public class FunSwitch extends View implements ValueAnimator.AnimatorUpdateListe
 
             @Override
             public void onAnimationUpdate(ValueAnimator animator) {
-                mCurrentColor = (int)animator.getAnimatedValue();
+                mCurrentColor = (int) animator.getAnimatedValue();
             }
 
         });
         colorAnimation.start();
 
     }
+
     @Override
     public void onAnimationUpdate(ValueAnimator animation) {
-        Log.e("TEST",animation.getAnimatedValue()+" ");
-        mAnimationFraction = (float)animation.getAnimatedValue();
+        Log.e("TEST", animation.getAnimatedValue() + " ");
+        mAnimationFraction = (float) animation.getAnimatedValue();
         invalidate();
 
     }
@@ -346,31 +367,73 @@ public class FunSwitch extends View implements ValueAnimator.AnimatorUpdateListe
         mIsDuringAnimation = true;
     }
 
-    public void setState(boolean open) {
-        mIsOpen = open;
-        refreshState();
-    }
     public void refreshState() {
         mCurrentColor = mIsOpen ? mOnBackgroundColor : mOffBackgroundColor;
         invalidate();
     }
 
     @Override
-    protected Parcelable onSaveInstanceState() {
-        Log.e("TEST","onSave");
-        Parcelable superState =  super.onSaveInstanceState();
+    public void setEnabled(boolean enabled) {
+        super.setEnabled(enabled);
+        invalidate();
+    }
+
+    /**
+     * 自动判断切换至相反的属性 : true -->false ;false -->true
+     */
+    public void toggle() {
+        setChecked(!mIsOpen);
+    }
+
+    /**
+     * 设置选中的状态（选中:true   非选中: false）
+     */
+    public void setChecked(boolean checked) {
+        mIsOpen = checked;
+        if (mIsOpen) {
+            mAnimationFraction = FACE_ANIM_MAX_FRACTION;
+        } else {
+            mAnimationFraction = 0;
+        }
+
+        if (mOnSwitchChangedListener != null) {
+            mOnSwitchChangedListener.onSwitchChange(this, mIsOpen);
+        }
+        refreshState();
+        invalidate();
+    }
+
+    /**
+     * 设置 switch 状态监听
+     */
+    public void setOnSwitchChangeListener(OnSwitchChangedListener listener) {
+        mOnSwitchChangedListener = listener;
+    }
+
+    /**
+     * switch 开关监听接口
+     */
+    public interface OnSwitchChangedListener {
+        public void onSwitchChange(FunSwitch funSwitch, boolean isChecked);
+    }
+
+
+    @Override
+    public Parcelable onSaveInstanceState() {
+        Log.e("TEST", "onSave");
+        Parcelable superState = super.onSaveInstanceState();
         SavedState ss = new SavedState(superState);
-        ss.isOpen = mIsOpen ? 1:0;
+        ss.isOpen = mIsOpen ? 1 : 0;
         return ss;
     }
 
     @Override
-    protected void onRestoreInstanceState(Parcelable state) {
-        Log.e("TEST","onRestore");
+    public void onRestoreInstanceState(Parcelable state) {
+        Log.e("TEST", "onRestore");
         SavedState ss = (SavedState) state;
         super.onRestoreInstanceState(state);
         boolean result = ss.isOpen == 1;
-        setState(result);
+        setChecked(result);
     }
 
     static class SavedState extends BaseSavedState {
@@ -390,6 +453,7 @@ public class FunSwitch extends View implements ValueAnimator.AnimatorUpdateListe
             super.writeToParcel(out, flags);
             out.writeInt(isOpen);
         }
+
         public static final Parcelable.Creator<SavedState> CREATOR
                 = new Parcelable.Creator<SavedState>() {
             @Override
